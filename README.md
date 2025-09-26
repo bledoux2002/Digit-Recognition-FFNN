294c4c78fbdb6e27faa9c3a5d24725cca9795be2
Benjamin Ledoux <bledoux2002@gmail.com>
2025-09-26 10:37:15 -0500

    adjusted parameters of model, going to add realtime updates to a ui based on user drawings
    3 -> 2 hidden layers, 128 and 64 nodes respectively

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Digit Recognition NN
 
 Using MNIST data to train and test a Neural Network.
+
+## Future Plans
+
+- Python graphic window to draw digits and update prediction in real time
---
+++ drawpad.py
@@ -0,0 +1,3 @@
+# Drawing program that will feed a 28x28 binary image into the neural network
+# maybe add controls to change topology of NN and retrain
+# maybe add visual of outputs as they change as well
